8ab5c637ab1a6bd555bfb10310d0ef811cf84aec
Alessandro Ranellucci <aar@cpan.org>
2011-02-06 14:00:44 +0000

    Import of AAR/MooseX-Collect-0.91 from CPAN.
    gitpan-cpan-distribution: MooseX-Collect
    gitpan-cpan-version:      0.91
    gitpan-cpan-path:         AAR/MooseX-Collect-0.91.tar.gz
    gitpan-cpan-author:       AAR
    gitpan-cpan-maturity:     released

--- lib/MooseX/Collect.pm
+++ lib/MooseX/Collect.pm
@@ -2,7 +2,7 @@ package MooseX::Collect;
 use strict;
 use warnings;
 
-our $VERSION = '0.90';
+our $VERSION = '0.91';
 
 sub import {
     my ($class, %args) = @_;
@@ -64,47 +64,45 @@ sub parse_collect {
     $init_args{from} ||= [qw(self superclasses roles)];
     $init_args{superclass_recurse} = 1 if !defined $init_args{superclass_recurse};
     $init_args{method_order} ||= 'standard';
+    my @providers = ($init_args{provider}, "_collect_" . $init_args{provider});
     
     my $meta = $package->meta;
     
     # if the caller package defines a provider method whose name
     # is equal to our accessor, we have to rename it in order to
     # install ours
-    my $caller_provider;
     if ($method_name eq $init_args{provider} && $meta->has_method($init_args{provider})) {
         if (!$meta->get_method($init_args{provider})->isa('MooseX::Collect::Method')) {
-            $caller_provider = $meta->remove_method($init_args{provider});
+            my $caller_provider = $meta->remove_method($init_args{provider});
             $meta->add_method("_collect_$method_name" => $caller_provider);
         }
     }
     
-    # a previous call to collect() may have renamed our provider
-    $caller_provider ||= $meta->get_method("_collect_" . $init_args{provider});
-    
     # generate the collection subroutine
     my $body = sub {
         my ($self, @args) = @_;
+        my $obj_meta = $self->meta;
         
         # let's retrieve our superclasses
         my (undef, @superclasses) = map $_->meta, 
-            grep $_->can('meta'), $meta->linearized_isa;
+            grep $_->can('meta'), $obj_meta->linearized_isa;
         @superclasses = ($superclasses[0]) if !$init_args{superclass_recurse};
         
         # let's find out which methods we need to call
         my @methods = ();
         foreach (@{$init_args{from}}) {
             if ($_ eq 'self') {
-                unshift @methods, $caller_provider if $caller_provider;
+                unshift @methods, grep !$_->isa('MooseX::Collect::Method'), 
+                    grep $_, map $obj_meta->get_method($_), @providers;
             } else {
                 my @metaclasses = ();
                 push @metaclasses, @superclasses if $_ eq 'superclasses';
                 push @metaclasses, grep !$_->isa('Moose::Meta::Role::Composite'), 
-                    $meta->calculate_all_roles_with_inheritance if $_ eq 'roles';
+                    $obj_meta->calculate_all_roles_with_inheritance if $_ eq 'roles';
                 @metaclasses = reverse @metaclasses if $init_args{method_order} eq 'reverse';
-                push @methods,
-                    map $_->get_method($init_args{provider}),
-                    grep $_->has_method($init_args{provider}),
-                    @metaclasses;
+                foreach my $metaclass (@metaclasses) {
+                    push @methods, grep $_, map $metaclass->get_method($_), @providers;
+                }
             }
         }
         
@@ -113,8 +111,8 @@ sub parse_collect {
         # call methods and retrieve results
         my @items = map {
             $init_args{context} eq 'list' 
-            ? ($_->execute(@args))
-            : scalar $_->execute(@args)
+            ? ($_->execute($self, @args))
+            : scalar $_->execute($self, @args)
         } @methods;
         
         # call user-provided subroutine
@@ -158,6 +156,7 @@ MooseX::Collect - provides method modifier for collecting method calls from role
     sub items () { qw/banana/ }
 
 
+
     package Foo;
     use Moose;
     use MooseX::Collect;
@@ -188,6 +187,7 @@ MooseX::Collect - provides method modifier for collecting method calls from role
     with qw(B C);
 
 
+
     my @items = $Foo->items;  # watermelon, apple, orange, banana
 
 
@@ -212,7 +212,7 @@ Any arguments passed to the newly created accessor will be passed to each method
 
 =item B<collect $method_name =E<gt> ()>
 
-This will install a method of a given C<$name> into the current class. You can pass a hash 
+This will install a method of a given C<$method_name> into the current class. You can pass a hash 
 of options to the function. Available options are listed below.
 As a shortcut, you can pass a coderef: it will be used as a I<collector> option (see below).
 
@@ -223,15 +223,18 @@ As a shortcut, you can pass a coderef: it will be used as a I<collector> option 
 This option sets the name of the method that will be searched in the inherited classes. This
 is useful if you want to use a different name for the local accessor. By default, the providers
 method name is the same of the accessor name (i.e. the $method_name you passed to I<collect>).
+The provider methods are called as object methods (that is, with the current object in $_[0]).
 
 =item I<from =E<gt> 'self' | 'superclass' | 'roles' | ARRAYREF>
 
 This options accepts a string containing one of the above values or an arrayref containing 
-one or more of them. Use it to specify which classes you want MooseX::Collect to search for
-provider methods. The order is relevant. The value I<self> enables searching of the method
-inside the current class too. The default value is I<self, superclass, roles>.
+one or more of them. Use it to specify the scope of your collection, i.e. which classes you 
+want MooseX::Collect to search for provider methods. The order is relevant. The value I<self> 
+enables searching of the method inside the class of the current object too. The default value 
+is I<self, superclass, roles>.
 
 =item I<method_order =E<gt> 'standard' | 'reverse'>
+
 =item I<method_order =E<gt> 'top_down' | 'bottom_up'>
 
 This option lets you reverse the default method resolution order, which is I<standard>
@@ -248,26 +251,63 @@ recursion), does not affect searching in roles because they have no hierarchy.
 =item I<context> =E<gt> 'scalar' | 'list'>
 
 This arguments lets you set the Perl context to use when calling the provider methods. By 
-default it's value is I<list>.
+default its value is I<list>.
 
 =item I<collector =E<gt> CODEREF>
 
 If you want to customize or filter the collection results, you can provide a custom coderef.
-It will receive the results in @_ and it's expected to return a list.
+It will receive the results in @_ and it's expected to return a list. It will be called as
+an object method, so $_[0] will contain your object.
 
 =back
 
 =back
+
+=head1 INHERITANCE
+
+Not surprisingly, collector methods are inherited by your subclasses. As you would expect,
+a subclass can override an inherited collector by defining a method with the same name.
+If you want to collect things from your subclasses, you need to set the I<provider> 
+attribute (see above) to a different name than your collector name:
+
+    package Foo;
+    use Moose;
+    use MooseX::Collect;
+    sub items { qw/apple/ }
+    collect 'get_items' => (
+        provider => 'items',
+    );
+    
+    package Bar;
+    use Moose;
+    extends 'Foo';
+    sub items { qw/orange/ }
+
+    package Baz;
+    my @items = Bar->new->get_items;  # orange, apple
+
+Note that the I<self> scope is relative to the object, and not to the class where the 
+collector is defined. So, if you add a C<from => 'self'> attribute in the above example,
+the call to C<get_items> will return "orange". The element "apple" will be available in 
+the I<superclasses> scope.
+
+=head1 CAVEATS
+
+Don't use the I<collect> method modifier to collect data from methods that are already
+defined as attribute accessors or extended by other method modifiers such as I<around>, 
+I<override> etc. or by another I<collect> modifier. In such cases, the behaviour of this 
+module is B<undefined> and B<unsupported>. You should avoid such usage until a proper
+policy (and the related test suite) is defined.
 
 =head1 SEE ALSO
 
 =over 4
 
-=item C<Moose>
+=item L<Moose>
 
-=item C<Moose::Role>
+=item L<Moose::Role>
 
-=item C<MooseX::ComposedBehavior>
+=item L<MooseX::ComposedBehavior>
 
 =back
 
@@ -275,6 +315,7 @@ It will receive the results in @_ and it's expected to return a list.
 
 Please report any bugs to C<bug-moosex-collect@rt.cpan.org>, or through the web
 interface at L<https://rt.cpan.org/Public/Bug/Report.html?Queue=MooseX-Collect>.
+The author will be happy to read your feedback.
 
 =head1 AUTHOR
 
